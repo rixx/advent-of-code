dd5913be995d1c0edbbe1e274f085df2099bea6d
Tobias Kunze <r@rixx.de>
2022-12-04 23:38:56 +0100

    [2015] Day 3 Part 2

--- 2015/a03.py
+++ 2015/a03.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from more_itertools import grouper
 from pathlib import Path
 
 
@@ -8,6 +9,20 @@ def parse_data():
     return data
 
 
+def get_direction(direction):
+    x = 0
+    y = 0
+    if direction == "^":
+        y += 1
+    elif direction == "v":
+        y -= 1
+    elif direction == "<":
+        x -= 1
+    elif direction == ">":
+        x += 1
+    return x, y
+
+
 def main_a():
     data = parse_data()
     x = 0
@@ -15,24 +30,31 @@ def main_a():
     chart = defaultdict(int)
     chart[(x, y)] += 1
     for direction in data:
-        if direction == "^":
-            y += 1
-        elif direction == "v":
-            y -= 1
-        elif direction == "<":
-            x -= 1
-        elif direction == ">":
-            x += 1
-        else:
-            continue
+        direction = get_direction(direction)
+        x += direction[0]
+        y += direction[1]
         chart[(x, y)] += 1
     print(len(chart.keys()))
 
 
 def main_b():
     data = parse_data()
+    santa = [0, 0]
+    robo = [0, 0]
+    chart = defaultdict(int)
+    chart[tuple(santa)] += 2
+    for s, r in grouper(data, 2):
+        direction = get_direction(s)
+        santa[0] += direction[0]
+        santa[1] += direction[1]
+        chart[tuple(santa)] += 1
+        direction = get_direction(r)
+        robo[0] += direction[0]
+        robo[1] += direction[1]
+        chart[tuple(robo)] += 1
+    print(len(chart.keys()))
 
 
 if __name__ == "__main__":
-    main_a()
-    # main_b()
+    # main_a()
+    main_b()
