da9054eba368c1068c527b0ae28ceef03f03decb
Tobias Kunze <r@rixx.de>
2022-12-04 22:11:19 +0100

    [2015] Day 3 Part 2

--- aoc03.py
+++ aoc03.py
@@ -1,5 +1,6 @@
-from pathlib import Path
+import more_itertools
 import string
+from pathlib import Path
 
 
 def parse_data():
@@ -9,8 +10,17 @@ def parse_data():
             yield (line[: length // 2], line[length // 2 :])
 
 
-def get_backpack_score(comp_a, comp_b):
-    common_letter = (set(comp_a) & set(comp_b)).pop()
+def parse_data_b():
+    with open(Path(__file__).stem + ".txt") as fp:
+        data = fp.readlines()
+    return more_itertools.grouper(data, 3)
+
+
+def get_backpack_score(comp_a, comp_b, comp_c=None):
+    common_letter = set(comp_a) & set(comp_b)
+    if comp_c:
+        common_letter &= set(comp_c)
+    common_letter = common_letter.pop()
     if common_letter in string.ascii_lowercase:
         return ord(common_letter) - 96
     return ord(common_letter) - 38
@@ -25,9 +35,13 @@ def main_a():
 
 
 def main_b():
-    data = parse_data()
+    groups = parse_data_b()
+    total = 0
+    for group in groups:
+        total += get_backpack_score(*group)
+    print(total)
 
 
 if __name__ == "__main__":
-    main_a()
-    # main_b()
+    # main_a()
+    main_b()
