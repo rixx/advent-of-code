6e825e6f16c65aa912c1c186d515dc66ffd41c87
Tobias Kunze <r@rixx.de>
2022-12-04 22:23:20 +0100

    [2015] Day 4 Part 2

--- aoc04.py
+++ aoc04.py
@@ -11,7 +11,7 @@ def parse_data():
         yield ((int(left[0]), int(left[1])), (int(right[0]), int(right[1])))
 
 
-def check_overlap(left, right):
+def check_full_overlap(left, right):
     if left[0] <= right[0] and left[1] >= right[1]:
         return True
     left, right = right, left
@@ -19,19 +19,34 @@ def check_overlap(left, right):
         return True
 
 
+def check_partial_overlap(left, right):
+    overlaps = (
+        (left[0], right[0], left[1]),
+        (left[0], right[1], left[1]),
+        (right[0], left[0], right[1]),
+        (right[0], left[1], right[1]),
+    )
+    for l, m, r in overlaps:
+        if l <= m <= r:
+            return True
+
+
 def main_a():
-    data = parse_data()
     count = 0
     for left, right in parse_data():
-        if check_overlap(left, right) or check_overlap(right, left):
+        if check_full_overlap(right, left):
             count += 1
     print(count)
 
 
 def main_b():
-    data = parse_data()
+    count = 0
+    for left, right in parse_data():
+        if check_partial_overlap(left, right):
+            count += 1
+    print(count)
 
 
 if __name__ == "__main__":
-    main_a()
-    # main_b()
+    # main_a()
+    main_b()
