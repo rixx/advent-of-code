3a130c915d285f7d50b17c0ba450f6ef18e5a2c3
Tobias Kunze <r@rixx.de>
2022-12-09 15:35:34 +0100

    [2015] Day 7 Part 1

---
+++ 2015/a07.py
@@ -0,0 +1,53 @@
+from pathlib import Path
+
+
+def parse_data():
+    result = {}
+    with open(Path(__file__).stem + ".txt") as fp:
+        for line in fp.readlines():
+            left, right = line.split("->")
+            result[right.strip()] = left.strip()
+    return result
+
+
+def evaluate(variable, lookup):
+    if isinstance(variable, int) or variable.isnumeric():
+        return int(variable)
+    equation = lookup[variable]
+    result = None
+    if isinstance(equation, int) or equation.isnumeric():
+        result = int(equation)
+    elif not " " in equation:
+        result = evaluate(equation, lookup)
+    elif equation.startswith("NOT"):
+        result = ~evaluate(equation.split()[-1], lookup)
+    else:
+        left, operator, right = equation.split()
+        left = evaluate(left, lookup)
+        right = evaluate(right, lookup)
+        if operator == "AND":
+            result = left & right
+        elif operator == "OR":
+            result = left | right
+        elif operator == "LSHIFT":
+            result = left << right
+        elif operator == "RSHIFT":
+            result = left >> right
+    lookup[variable] = result
+    return result
+
+
+def main_a(data):
+    print(evaluate("a", data))
+
+
+def main_b(data):
+    pass
+
+
+if __name__ == "__main__":
+    data = parse_data()
+    print("##### Part 1 #####")
+    main_a(data)
+    print("\n##### Part 2 #####")
+    main_b(data)
