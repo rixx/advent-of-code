09eb38fff439befe2b7b559ded98d54f8d643cf1
Tobias Kunze <r@rixx.de>
2022-12-11 22:41:03 +0100

    [2022] Day 11 Part 2 oops lol

--- 2022/aoc11.py
+++ 2022/aoc11.py
@@ -1,4 +1,8 @@
+from copy import deepcopy
 from pathlib import Path
+import sys
+
+MAX_WORRY = 11 * 7 * 13 * 5 * 3 * 17 * 2 * 19
 
 
 def parse_data():
@@ -8,7 +12,7 @@ def parse_data():
 
 
 class Monkey:
-    def __init__(self, instructions):
+    def __init__(self, instructions, part_1=True):
         name, starting, operation, test, true, false = instructions.strip().split("\n")
         self.name = name.split()[-1].strip(":")
         self.items = [int(item) for item in starting.split(":")[-1].split(",")]
@@ -17,6 +21,7 @@ class Monkey:
         self.output_true = int(true.split()[-1])
         self.output_false = int(false.split()[-1])
         self.inspection_counter = 0
+        self.part_1 = part_1
 
     def inspect(self):
         self.inspection_counter += 1
@@ -26,10 +31,14 @@ class Monkey:
             worry = int(left) + int(right)
         elif operator == "*":
             worry = int(left) * int(right)
-        worry = int(worry / 3)
+        if self.part_1:
+            worry = int(int(worry) / 3)
+        worry = worry % MAX_WORRY
         return {
             "item": worry,
-            "move_to": self.output_true if self.test(worry) else self.output_false,
+            "move_to": self.output_true
+            if worry % self.test_divisible == 0
+            else self.output_false,
         }
 
     def run_round(self):
@@ -37,9 +46,6 @@ class Monkey:
         while self.items:
             result.append(self.inspect())
         return result
-
-    def test(self, number):
-        return number % self.test_divisible == 0
 
     def add_item(self, item: int):
         self.items.append(item)
@@ -53,6 +59,13 @@ def run_round(monkeys):
     return monkeys
 
 
+def get_result(monkeys):
+    monkey_list = sorted(
+        monkeys.values(), key=lambda m: m.inspection_counter, reverse=True
+    )
+    print(monkey_list[0].inspection_counter * monkey_list[1].inspection_counter)
+
+
 def main_a(data):
     monkey_list = [Monkey(instructions) for instructions in data]
     monkeys = {int(monkey.name): monkey for monkey in monkey_list}
@@ -62,14 +75,19 @@ def main_a(data):
         print(
             f"Monkey {monkey.name} inspected items {monkey.inspection_counter} times."
         )
-    monkey_list = sorted(
-        monkeys.values(), key=lambda m: m.inspection_counter, reverse=True
-    )
-    print(monkey_list[0].inspection_counter * monkey_list[1].inspection_counter)
+    get_result(monkeys)
 
 
 def main_b(data):
-    pass
+    monkey_list = [Monkey(instructions, part_1=False) for instructions in data]
+    monkeys = {int(monkey.name): monkey for monkey in monkey_list}
+    for _ in range(10_000):
+        monkeys = run_round(monkeys)
+    for monkey in monkeys.values():
+        print(
+            f"Monkey {monkey.name} inspected items {monkey.inspection_counter} times."
+        )
+    get_result(monkeys)
 
 
 if __name__ == "__main__":
