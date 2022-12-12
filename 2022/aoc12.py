f96408168ce3699c7d282d24bc1e76d01a644e7e
Tobias Kunze <r@rixx.de>
2022-12-12 17:55:12 +0100

    [2022] Day 12 Part 2, v slow but whatev

--- 2022/aoc12.py
+++ 2022/aoc12.py
@@ -10,15 +10,17 @@ MAP[START] = 0
 MAP[END] = 27
 
 
-def parse_data():
+def parse_data(start_cutoff=0):
     result = []
-    start_coords = None
+    start_coords = []
     end_coords = None
     with open(Path(__file__).stem + ".txt") as fp:
         for x_coord, line in enumerate(fp.readlines()):
             line_result = [MAP[letter] for letter in line.strip()]
             with suppress(ValueError):
-                start_coords = (x_coord, line_result.index(0))
+                for index in range(len(line_result)):
+                    if line_result[index] <= start_cutoff:
+                        start_coords.append((x_coord, index))
             with suppress(ValueError):
                 end_coords = (x_coord, line_result.index(27))
             result.append(line_result)
@@ -65,17 +67,28 @@ def find_way(grid, start, end):
 
 
 def main_a(grid, start, end):
+    start = start[0]
     path, nodes = find_way(grid, start, end)
     print(path[end])
 
 
-def main_b(grid, start, end):
-    pass
+def main_b(grid, starts, end):
+    min_result = 99999999
+    print(f"Found {len(starts)} starts")
+    for index, start in enumerate(starts):
+        path, nodes = find_way(grid, start, end)
+        way = path[end]
+        if way < min_result:
+            min_result = way
+        if index % 10 == 0:
+            print(f"Run {index}, result {min_result}")
+    print(min_result)
 
 
 if __name__ == "__main__":
     data = parse_data()
     print("##### Part 1 #####")
     main_a(*data)
+    data = parse_data(start_cutoff=1)
     print("\n##### Part 2 #####")
     main_b(*data)
