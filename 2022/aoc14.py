46599dcab3db2c430da044ef3290f09491c574d8
Tobias Kunze <r@rixx.de>
2022-12-14 19:25:38 +0100

    [2022] Day 14 Part 2 Oops we needed some debugging

--- 2022/aoc14.py
+++ 2022/aoc14.py
@@ -5,6 +5,11 @@ import more_itertools
 AIR = 0
 ROCK = 1
 SAND = 2
+PRINT_MAP = {
+    AIR: ".",
+    ROCK: "#",
+    SAND: "o",
+}
 START = (500, 0)
 
 
@@ -42,10 +47,13 @@ def parse_data():
 
 
 class Field:
-    def __init__(self, data):
+    def __init__(self, data, bottom_offset=0):
         self.field = data
         self.sand_generated = 0
         self.largest_y = self.get_largest_y()
+        self.cutoff = None
+        if bottom_offset:
+            self.cutoff = self.largest_y + bottom_offset - 1
 
     def get_largest_y(self):
         values = {y for x, y in self.field.keys()}
@@ -55,23 +63,41 @@ class Field:
     def sand_count(self):
         return len([v for v in self.field.values() if v == SAND])
 
+    def is_free(self, x, y):
+        if self.cutoff and y == self.cutoff:
+            return False
+        return self.field[x, y] == AIR
+
     def generate_sand(self):
         x, y = START
+        if not self.is_free(x, y):
+            return False
         while True:
-            if self.field[x, y + 1] == AIR:
+            if self.is_free(x, y + 1):
                 y += 1
-                if y > self.largest_y:
+                if y > self.largest_y and not self.cutoff:
                     return False
-            elif self.field[x - 1, y + 1] == AIR:
+            elif self.is_free(x - 1, y + 1):
                 x -= 1
                 y += 1
-            elif self.field[x + 1, y + 1] == AIR:
+            elif self.is_free(x + 1, y + 1):
                 x += 1
                 y += 1
             else:
                 self.field[(x, y)] = SAND
                 self.sand_generated += 1
                 return True
+
+    def print(self):
+        y_values = {y for x, y in self.field.keys()}
+        x_values = {x for x, y in self.field.keys()}
+        x_range = min(x_values) - 1, max(x_values) + 3
+        y_range = 0, max(y_values) + 1
+        for y in range(*y_range):
+            line = ""
+            for x in range(*x_range):
+                line += PRINT_MAP[self.field[x, y]]
+            print(line)
 
     def run_to_completion(self):
         while True:
@@ -80,11 +106,13 @@ class Field:
 
 
 def main_a(data):
-    print(Field(data).run_to_completion())
+    field = Field(data)
+    print(field.run_to_completion())
 
 
 def main_b(data):
-    pass
+    field = Field(data, bottom_offset=2)
+    print(field.run_to_completion())
 
 
 if __name__ == "__main__":
